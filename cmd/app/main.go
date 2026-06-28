91db8ca768e799fa1ab02ee0b1450d477d13ff17
lipkerton <petr.petuxof@yandex.ru>
2026-06-28 15:41:58 +0300

    server params

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/lipkerton/subscription-service1/internal/config"
 	httptransport "github.com/lipkerton/subscription-service1/internal/transport/http"
@@ -16,8 +21,36 @@ func main() {
 	}
 	r := httptransport.NewRouter()
 	addr := fmt.Sprintf(":%d", cfg.App.Port)
-	log.Printf("server started at %s", addr)
-	if err := http.ListenAndServe(addr, r); err != nil {
-		log.Fatal(err)
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
+	go func() {
+		log.Printf("server started at %s", addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(
+		stop,
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	<-stop
+
+	log.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed: %v", err)
+	}
+	log.Println("server stopped gracefully")
 }
