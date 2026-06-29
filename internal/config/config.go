083752488de7152403a9a8840a264ab144ae70d4
lipkerton <petr.petuxof@yandex.ru>
2026-06-29 09:00:35 +0300

    postgres config

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,26 +9,68 @@ import (
 )
 
 type Config struct {
-	App AppConfig
+	App      AppConfig
+	Postgres PostgresConfig
 }
 
 type AppConfig struct {
 	Port int
 }
 
+type PostgresConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
 func Load() (*Config, error) {
 	_ = godotenv.Load()
 
-	port, err := getEnvAsInt("APP_PORT", 8080)
+	appPort, err := getEnvAsInt("APP_PORT", 8080)
 	if err != nil {
 		return nil, fmt.Errorf("get app port: %w", err)
 	}
+
+	postgresPort, err := getEnvAsInt("POSTGRES_PORT", 5432)
+	if err != nil {
+		return nil, fmt.Errorf("get postgres port: %w", err)
+	}
 	return &Config{
 		App: AppConfig{
-			Port: port,
+			Port: appPort,
+		},
+		Postgres: PostgresConfig{
+			Host:     getEnv("POSTGRES_HOST", "localhost"),
+			Port:     postgresPort,
+			User:     getEnv("POSTGRES_USER", "postgres"),
+			Password: getEnv("POSTGRES_PASSWORD", "postgres"),
+			DBName:   getEnv("POSTGRES_DB", "subscriptions_db"),
+			SSLMode:  getEnv("POSTGRES_SSL_MODE", "disable"),
 		},
 	}, nil
+}
 
+func (c PostgresConfig) DSN() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		c.User,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.DBName,
+		c.SSLMode,
+	)
+}
+
+func getEnv(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
 
 func getEnvAsInt(key string, defaultValue int) (int, error) {
